b0244726aed5e2f3038edf0ec45302a64e5b7733
lsamano <leizl.samano@gmail.com>
2020-03-19 17:33:32 -0400

    crop preferences added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+Season.destroy_all
+Crop.destroy_all
+Flower.destroy_all
+Like.destroy_all
+Hate.destroy_all
+
 
 spring = Season.create(name: "spring")
 summer = Season.create(name: "summer")
@@ -15,6 +21,9 @@ cabbage = Crop.create(
   regrows: false,
   store_level: 1
 )
+cabbage.liked_seasons << spring
+cabbage.liked_seasons << fall
+
 pink_turnip = Crop.create(
   name: "pink turnip",
   grow_time: 11,
@@ -22,6 +31,9 @@ pink_turnip = Crop.create(
   regrows: false,
   store_level: 1
 )
+pink_turnip.liked_seasons << spring
+pink_turnip.hated_seasons << winter
+
 pink_melon = Crop.create(
   name: "pink melon",
   grow_time: 7,
@@ -29,6 +41,10 @@ pink_melon = Crop.create(
   regrows: true,
   store_level: 1
 )
+pink_melon.liked_seasons << spring
+pink_melon.liked_seasons << summer
+pink_melon.hated_seasons << winter
+
 onion = Crop.create(
   name: "pink melon",
   grow_time: 18,
@@ -36,6 +52,8 @@ onion = Crop.create(
   regrows: false,
   store_level: 1
 )
+onion.liked_seasons << fall
+
 pumpkin = Crop.create(
   name: "pumpkin",
   grow_time: 7,
@@ -43,6 +61,10 @@ pumpkin = Crop.create(
   regrows: true,
   store_level: 1
 )
+pumpkin.liked_seasons << spring
+pumpkin.liked_seasons << summer
+pumpkin.hated_seasons << winter
+
 cucumber = Crop.create(
   name: "cucumber",
   grow_time: 5,
@@ -50,6 +72,10 @@ cucumber = Crop.create(
   regrows: true,
   store_level: 1
 )
+cucumber.liked_seasons << spring
+cucumber.liked_seasons << summer
+cucumber.hated_seasons << winter
+
 corn = Crop.create(
   name: "corn",
   grow_time: 7,
@@ -57,6 +83,10 @@ corn = Crop.create(
   regrows: false,
   store_level: 1
 )
+corn.liked_seasons << spring
+corn.liked_seasons << summer
+corn.hated_seasons << winter
+
 tomato = Crop.create(
   name: "tomato",
   grow_time: 7,
@@ -64,6 +94,10 @@ tomato = Crop.create(
   regrows: true,
   store_level: 1
 )
+tomato.liked_seasons << spring
+tomato.liked_seasons << summer
+tomato.hated_seasons << winter
+
 eggplant = Crop.create(
   name: "eggplant",
   grow_time: 7,
@@ -71,6 +105,10 @@ eggplant = Crop.create(
   regrows: true,
   store_level: 1
 )
+eggplant.liked_seasons << spring
+eggplant.liked_seasons << summer
+eggplant.hated_seasons << winter
+
 yam = Crop.create(
   name: "yam",
   grow_time: 8,
@@ -78,6 +116,10 @@ yam = Crop.create(
   regrows: false,
   store_level: 1
 )
+yam.liked_seasons << summer
+yam.liked_seasons << fall
+yam.hated_seasons << winter
+
 potato = Crop.create(
   name: "potato",
   grow_time: 7,
@@ -85,6 +127,10 @@ potato = Crop.create(
   regrows: false,
   store_level: 1
 )
+potato.liked_seasons << spring
+potato.liked_seasons << summer
+potato.hated_seasons << winter
+
 carrot = Crop.create(
   name: "carrot",
   grow_time: 10,
@@ -92,6 +138,8 @@ carrot = Crop.create(
   regrows: false,
   store_level: 1
 )
+carrot.liked_seasons << fall
+
 green_pepper = Crop.create(
   name: "green pepper",
   grow_time: 7,
@@ -99,6 +147,9 @@ green_pepper = Crop.create(
   regrows: true,
   store_level: 1
 )
+green_pepper.liked_seasons << summer
+green_pepper.hated_seasons << winter
+
 spinach = Crop.create(
   name: "spinach",
   grow_time: 2,
@@ -106,6 +157,8 @@ spinach = Crop.create(
   regrows: false,
   store_level: 1
 )
+spinach.liked_seasons << fall
+
 turnip = Crop.create(
   name: "turnip",
   grow_time: 3,
@@ -113,6 +166,9 @@ turnip = Crop.create(
   regrows: false,
   store_level: 1
 )
+turnip.liked_seasons << summer
+turnip.liked_seasons << fall
+
 radish = Crop.create(
   name: "radish",
   grow_time: 5,
@@ -120,6 +176,9 @@ radish = Crop.create(
   regrows: false,
   store_level: 1
 )
+radish.liked_seasons << fall
+radish.hated_seasons << summer
+
 leek = Crop.create(
   name: "leek",
   grow_time: 9,
@@ -127,6 +186,10 @@ leek = Crop.create(
   regrows: false,
   store_level: 1
 )
+leek.liked_seasons << spring
+leek.liked_seasons << fall
+leek.hated_seasons << summer
+
 bok_choy = Crop.create(
   name: "bok choy",
   grow_time: 5,
@@ -134,6 +197,9 @@ bok_choy = Crop.create(
   regrows: false,
   store_level: 1
 )
+bok_choy.liked_seasons << fall
+bok_choy.hated_seasons << summer
+
 hot_hot_fruit = Crop.create(
   name: "hot hot fruit",
   grow_time: 31,
@@ -141,6 +207,9 @@ hot_hot_fruit = Crop.create(
   regrows: false,
   store_level: 1
 )
+hot_hot_fruit.liked_seasons << winter
+hot_hot_fruit.hated_seasons << summer
+
 golden_cabbage = Crop.create(
   name: "golden cabbage",
   grow_time: 90,
@@ -148,6 +217,9 @@ golden_cabbage = Crop.create(
   regrows: false,
   store_level: 1
 )
+golden_cabbage.liked_seasons << winter
+golden_cabbage.hated_seasons << summer
+
 golden_pumpkin = Crop.create(
   name: "golden pumpkin",
   grow_time: 70,
@@ -155,6 +227,9 @@ golden_pumpkin = Crop.create(
   regrows: true,
   store_level: 1
 )
+golden_pumpkin.liked_seasons << winter
+golden_pumpkin.hated_seasons << summer
+
 golden_potato = Crop.create(
   name: "golden potato",
   grow_time: 50,
@@ -162,6 +237,9 @@ golden_potato = Crop.create(
   regrows: false,
   store_level: 1
 )
+golden_potato.liked_seasons << winter
+golden_potato.hated_seasons << summer
+
 golden_turnip = Crop.create(
   name: "golden turnip",
   grow_time: 90,
@@ -169,6 +247,9 @@ golden_turnip = Crop.create(
   regrows: false,
   store_level: 1
 )
+golden_turnip.liked_seasons << winter
+golden_turnip.hated_seasons << summer
+
 strawberry = Crop.create(
   name: "strawberry",
   grow_time: 16,
@@ -176,6 +257,9 @@ strawberry = Crop.create(
   regrows: true,
   store_level: 1
 )
+strawberry.liked_seasons << spring
+strawberry.liked_seasons << fall
+
 pineapple = Crop.create(
   name: "pineapple",
   grow_time: 28,
@@ -183,6 +267,10 @@ pineapple = Crop.create(
   regrows: true,
   store_level: 1
 )
+pineapple.liked_seasons << summer
+pineapple.hated_seasons << spring
+pineapple.hated_seasons << fall
+pineapple.hated_seasons << winter
 
 # Miscellaneous
 fodder = Crop.create(
@@ -192,6 +280,7 @@ fodder = Crop.create(
   regrows: true,
   store_level: 1
 )
+
 grapes = Crop.create(
   name: "grapes",
   grow_time: 50,
@@ -199,6 +288,8 @@ grapes = Crop.create(
   regrows: true,
   store_level: 1
 )
+grapes.hated_seasons << winter
+
 orange = Crop.create(
   name: "orange",
   grow_time: 22,
@@ -206,6 +297,8 @@ orange = Crop.create(
   regrows: true,
   store_level: 1
 )
+orange.hated_seasons << winter
+
 apple = Crop.create(
   name: "apple",
   grow_time: 62,
@@ -213,6 +306,8 @@ apple = Crop.create(
   regrows: true,
   store_level: 1
 )
+apple.hated_seasons << winter
+
 twinkle_tree = Crop.create(
   name: "twinkle tree",
   grow_time: 38,
@@ -220,6 +315,8 @@ twinkle_tree = Crop.create(
   regrows: false,
   store_level: 1
 )
+twinkle_tree.hated_seasons << winter
+
 shield_flower = Crop.create(
   name: "shield flower",
   grow_time: 12,
@@ -227,6 +324,7 @@ shield_flower = Crop.create(
   regrows: false,
   store_level: 1
 )
+
 sword_flower = Crop.create(
   name: "sword flower",
   grow_time: 12,
@@ -234,6 +332,7 @@ sword_flower = Crop.create(
   regrows: false,
   store_level: 1
 )
+
 dungeon_flower = Crop.create(
   name: "dungeon flower",
   grow_time: 18,
