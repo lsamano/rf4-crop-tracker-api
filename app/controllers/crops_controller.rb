1d80f9906fbc09931551fd1bcd4d65cd8446103f
lsamano <leizl.samano@gmail.com>
2020-03-22 09:25:32 -0400

    update-level endpoint added

--- app/controllers/crops_controller.rb
+++ app/controllers/crops_controller.rb
@@ -8,4 +8,17 @@ class CropsController < ApplicationController
     crop = Crop.find_by(name: params[:name].titleize.downcase)
     render json: crop, include: [:liked_seasons, :hated_seasons]
   end
+
+  def update_level
+    crop = Crop.find_by(name: params[:name])
+    crop.update(store_level: params[:new_level])
+    if crop.valid?
+      render json: crop
+    else
+      render json: {
+        error: "An error has occured.",
+        full_messages: crop.errors.full_messages
+        }, status :unacceptable
+    end
+  end
 end
